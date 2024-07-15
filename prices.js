916c09aea8a1063f7b53ccfe59de5a419b530696
Alex Straton <alexandrastraton1@gmail.com>
2024-07-15 10:30:06 +0100

    created file structure and base products with unit prices and offers

---
+++ prices.js
@@ -0,0 +1,8 @@
+const products = {
+  A: 50,
+  B: 35,
+  C: 25,
+  D: 12,
+};
+
+module.exports = products;
---
+++ specialPrice.js
@@ -0,0 +1,6 @@
+const specialPrices = {
+  A: { quantity: 3, price: 140 },
+  B: { quantity: 2, price: 60 },
+};
+
+module.exports = specialPrices;
